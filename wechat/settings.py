d76d9926545e2b9a17a48c6a538a60f8e74c643b
root <root@localhost.localdomain>
2019-10-03 19:19:14 +0800

    提交

--- wechat/settings.py
+++ wechat/settings.py
@@ -113,19 +113,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static_dist') 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'front','dist')
 ]
